8a12b318a01c27ae26dcc92a8a90be7d6a552dcf
rdolan <ryanddolan@hotmail.com>
2022-02-10 16:52:01 -0600

    online api for AcoustID

---
+++ AudioPlayer/AudioPlayer/Component/AcoustIDClient.cs
@@ -0,0 +1,25 @@
+﻿using AcoustID.Web;
+using AudioPlayer.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioPlayer.Component
+{
+    public static class AcoustIDClient
+    {
+        /// <summary>
+        /// Calculates library entry by audio fingerprint using an online api.
+        /// </summary>
+        public static async Task<ILibraryMetaEntry> IdentifyFingerprint(ILibraryEntry entry)
+        {
+            var context = new AcoustID.ChromaContext();
+
+            using (var service = new LookupService())
+            {
+
+            }
+        }
+    }
+}
